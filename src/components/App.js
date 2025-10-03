b6a1080753f8cc203ed1bfd412afcc4f9c3ea02d
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-03 20:47:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <Router>
+        <Routes>
+          {/* Homepage showing list of items */}
+          <Route path="/" element={<ItemList />} />
+          {/* Dynamic route for item details */}
+          <Route path="/items/:id" element={<ItemDetail />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
